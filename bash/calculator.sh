9937ce548ef62be36f74bcc6b93a6923967039a2
dad <cam.t.watson@gmail.com>
2025-09-05 14:47:26 -0400

    add var

--- bash/calculator.sh
+++ bash/calculator.sh
@@ -1,5 +1,6 @@
-while : 
-do
+res=0
+
+while :; do
   read -p "what would you like to do?
   1. Add
   2. Subtract
@@ -8,34 +9,29 @@ do
   5. Quit
   
   " res
-    if [[ $res -eq 1 ]]
-       then
-          read -p "What is the first number? " num1
-          read -p "what is your second number? " num2
-          ans=$(echo "$num1 + $num2" | bc)
-          echo "The answer is $ans"
-    elif [[ $res -eq 2 ]]
-       then
-          read -p "What is the first number? " num1
-          read -p "what is your second number? " num2
-          ans=$(echo "$num1 - $num2" | bc)
-          echo "The answer is $ans"  
-    elif [[ $res -eq 3 ]]
-       then
-          read -p "What is the first number? " num1
-          read -p "what is your second number? " num2
-          ans=$(echo "$num1 * $num2" | bc)
-          echo "The answer is $ans"
-    elif [[ $res -eq 4 ]]
-       then
-          read -p "What is the first number? " num1
-          read -p "what is your second number? " num2
-          ans=$(echo "$num1 / $num2" | bc)
-          echo "The answer is $ans"
-    elif [[ $res -eq 5 ]]
-       then
-          break
-    else
-       echo "Please type a number 1-5 "
-    fi
+  if [[ $res -eq 1 ]]; then
+    read -p "What is the first number? " num1
+    read -p "what is your second number? " num2
+    ans=$(echo "$num1 + $num2" | bc)
+    echo "The answer is $ans"
+  elif [[ $res -eq 2 ]]; then
+    read -p "What is the first number? " num1
+    read -p "what is your second number? " num2
+    ans=$(echo "$num1 - $num2" | bc)
+    echo "The answer is $ans"
+  elif [[ $res -eq 3 ]]; then
+    read -p "What is the first number? " num1
+    read -p "what is your second number? " num2
+    ans=$(echo "$num1 * $num2" | bc)
+    echo "The answer is $ans"
+  elif [[ $res -eq 4 ]]; then
+    read -p "What is the first number? " num1
+    read -p "what is your second number? " num2
+    ans=$(echo "$num1 / $num2" | bc)
+    echo "The answer is $ans"
+  elif [[ $res -eq 5 ]]; then
+    break
+  else
+    echo "Please type a number 1-5 "
+  fi
 done
